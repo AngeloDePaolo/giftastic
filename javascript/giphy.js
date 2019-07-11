317dbf7b6509fa0392fd850925d3dc83f3f1dc1f
Angelo DePaolo <adepaolo@gmail.com>
2019-07-11 16:39:21 -0700

    incorporated giphy API AJAX request

--- javascript/giphy.js
+++ javascript/giphy.js
@@ -1,8 +1,28 @@
-var topics = ["The Matrix", "The Shawsank Redemption", "Die Hard", "Star Trek", "Star Wars"];
+$(document).ready(function () {
 
-function buttonZone() {
+    var topics = ["The Matrix", "The Shawsank Redemption", "Die Hard", "Star Trek", "Star Wars"];
 
+    function buttonStaging() {
+        $("#buttons").empty();
+        for (var i = 0; i < topics.length; i++) {
+            var button = $("<button>");
+            button.addClass("movie");
+            button.attr("data-name", topics[i]);
+            button.text(topics[i]);
+            $("#buttons").append(button);
+        }
+    }
 
+    $("#add-movie").on("click", function (event) {
+        event.preventDefault();
 
+        var movie = $("#searchText").val().trim();
+        topics.push(movie);
+        console.log(movie);
 
-}
+        buttonStaging();
+    });
+
+    buttonStaging();
+
+});
